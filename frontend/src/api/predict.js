5982731beeaa35345c880ef9b6cf18ad0735faf3
Amardip <96110469+Ak2616@users.noreply.github.com>
2025-12-01 17:22:39 +0530

    fix: make necessary changes in frotend for deployment

--- frontend/src/api/predict.js
+++ frontend/src/api/predict.js
@@ -1,5 +1,5 @@
 export async function predictScore(score) {
-  const res = await fetch("http://localhost:8000/predict", {
+  const res = await fetch("https://lords-backend-production.up.railway.app/predict", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ score: Number(score) })
